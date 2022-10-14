8a9d004a92e4b5f630478fbcb2af6e62bd19eb43
R. Jordan Crouser <jcrouser@smith.edu>
2022-10-14 11:40:06 -0400

    Create Pumpkin.java

---
+++ Pumpkin.java
@@ -0,0 +1,26 @@
+/*
+ * Pumpkin class (template)
+ * Assignment 5: Bringing it All Together
+ * @author R. Jordan Crouser + CSC120 (Fall '22))
+ * @version 13 October 2022
+ */
+public class Pumpkin {
+    
+    /*
+     * Prints ASCII art depicting a jack-o'-lantern to the console
+     */
+    public void display() {
+
+        System.out.println("              ___");
+        System.out.println("           ___)__|_");
+        System.out.println("      .-*'          '*-,");
+        System.out.println("     /      /|   |\\     \\ ");
+        System.out.println("    ;      /_|   |_\\     ;");
+        System.out.println("    ;   |\\           /|  ;");
+        System.out.println("    ;   | ''--...--'' |  ;");
+        System.out.println("     \\  ''---.....--''  /");
+        System.out.println("     ''*-.,_______,.-*'  ");  
+
+    }
+
+}
