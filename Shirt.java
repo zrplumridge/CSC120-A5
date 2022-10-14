7921983f6fdacf4f9c707077db24058e482718f5
R. Jordan Crouser <jcrouser@smith.edu>
2022-10-14 11:44:40 -0400

    Create Shirt.java

---
+++ Shirt.java
@@ -0,0 +1,21 @@
+/*
+ * Shirt class (template)
+ * Assignment 5: Bringing it All Together
+ * @author R. Jordan Crouser + CSC120 (Fall '22))
+ * @version 13 October 2022
+ */
+public class Shirt {
+
+    /*
+     * TODO: Modify this method to print ASCII Art Shirt
+     */
+    public void display() {
+        System.out.println("Shirt?);
+    }
+
+    /* main method (for testing) */
+    public static void main(String[] args) {
+        Shirt myShirt = new Shirt();
+        myShirt.display();
+    }
+}
