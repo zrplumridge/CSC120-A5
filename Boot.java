ab177641d8c200b2e67c7f3534515bcfbc7d3d93
R. Jordan Crouser <jcrouser@smith.edu>
2022-10-14 11:50:10 -0400

    Create Boot.java

---
+++ Boot.java
@@ -0,0 +1,36 @@
+/*
+ * Boot class (template)
+ * Assignment 5: Bringing it All Together
+ * @author R. Jordan Crouser + CSC120 (Fall '22))
+ * @version 13 October 2022
+ */
+public class Boot {
+  
+    private String direction;
+  
+    /* Constructor */
+    public Boot(String direction) {
+        this.direction = direction.toLowerCase();
+    }
+
+    /*
+     * TODO: Modify this method to print ASCII Art Boot
+     */
+    public void display() {
+        if (this.direction.equals("left")) {
+          System.out.println("Left boot?");
+        } else if (this.direction.equals("right")) {
+          System.out.println("Right boot?");
+        } else {
+          System.out.println("Which boot?");
+        }
+    }
+
+    /* main method (for testing) */
+    public static void main(String[] args) {
+        Boot myLeftBoot = new Boot("left");
+        Boot myRightBoot = new Boot("right");
+        myLeftBoot.display();
+        myRightBoot.display();
+    }
+}
