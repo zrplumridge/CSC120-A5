3458ca1dfbafefc38483aa78b1b176c08c324444
R. Jordan Crouser <jcrouser@smith.edu>
2022-10-14 11:44:03 -0400

    Create Pants.java

---
+++ Pants.java
@@ -0,0 +1,21 @@
+/*
+ * Pants class (template)
+ * Assignment 5: Bringing it All Together
+ * @author R. Jordan Crouser + CSC120 (Fall '22))
+ * @version 13 October 2022
+ */
+public class Pants {
+
+    /*
+     * TODO: Modify this method to print ASCII Art Pants
+     */
+    public void display() {
+        System.out.println("Pants?);
+    }
+
+    /* main method (for testing) */
+    public static void main(String[] args) {
+        Pants myPants = new Pants();
+        myPants.display();
+    }
+}
